24f079ca728746c47f4b3f66d8e603954f8018eb
Lucai994 <luca.muratori.94@hotmail.it>
2022-03-01 14:48:02 +0100

    commit

--- index.js
+++ index.js
@@ -75,3 +75,78 @@ function fetchBehemoth() {
       console.log(error);
     });
 }
+
+function howManyEminem() {
+  const url = fetch(
+    "https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem"
+  );
+  url
+    .then((response) => response.json())
+    .then((uniqueSong) => {
+      let arr = [];
+      for (let i = 0; i < uniqueSong.data.length; i++) {
+        const data = uniqueSong.data[i].title;
+        let isUnique = true;
+        for (let j = 0; j < arr.length; j++) {
+          if (arr[j] === data) {
+            isUnique = false;
+            break;
+          }
+        }
+        if (isUnique) {
+          arr.push(data);
+        }
+      }
+      console.log(`There are ${arr.length} unique song`);
+    });
+}
+
+function howManyMetallica() {
+  const url = fetch(
+    "https://striveschool-api.herokuapp.com/api/deezer/search?q=Metallica"
+  );
+  url
+    .then((response) => response.json())
+    .then((uniqueSong) => {
+      let arr = [];
+      for (let i = 0; i < uniqueSong.data.length; i++) {
+        const data = uniqueSong.data[i].title;
+        let isUnique = true;
+        for (let j = 0; j < arr.length; j++) {
+          if (arr[j] === data) {
+            isUnique = false;
+            break;
+          }
+        }
+        if (isUnique) {
+          arr.push(data);
+        }
+      }
+      console.log(`There are ${arr.length} unique song`);
+    });
+}
+
+function howManyBehemoth() {
+  const url = fetch(
+    "https://striveschool-api.herokuapp.com/api/deezer/search?q=Behemoth"
+  );
+  url
+    .then((response) => response.json())
+    .then((uniqueSong) => {
+      let arr = [];
+      for (let i = 0; i < uniqueSong.data.length; i++) {
+        const data = uniqueSong.data[i].title;
+        let isUnique = true;
+        for (let j = 0; j < arr.length; j++) {
+          if (arr[j] === data) {
+            isUnique = false;
+            break;
+          }
+        }
+        if (isUnique) {
+          arr.push(data);
+        }
+      }
+      console.log(`There are ${arr.length} unique song`);
+    });
+}
